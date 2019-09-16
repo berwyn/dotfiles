41c35fabab449b35ff3cf74f447d02aa91337c46
berwyn <berwyn.codeweaver@gmail.com>
2019-09-16 10:39:15 -0400

    Cleanup

--- setup.sh
+++ setup.sh
@@ -11,11 +11,30 @@ whisper() {
 setup_fish() {
     say "Setting up Fish"
     sudo chsh -s /usr/local/bin/fish $(whoami)
-    fish -c "fundle init && fundle install"
+    fish -C "fundle init && fundle install"
 }
 
 setup_macos() {
     say "Setting up macOS"
+    whisper "Disabling Gatekeeper"
+    sudo spctl --master-disable
+
+    whisper "Setting symlinks"
+    FILES=(".tmux.conf" ".vimrc" ".vim")
+    for FILE in ${FILES[*]}; do
+        ln -Fihsv "$PWD/$FILE" "$HOME/$FILE"
+    done
+
+    # macOS' `ln` behaves weird, so
+    ln -Fihsv "$PWD/.config" $HOME
+    # Specifically, trying to link to `$HOME/.config` creates `$HOME/.config/.config`
+
+    whisper "Setting up Homebrew"
+    if ! command -v brew >/dev/null 2>&1; then
+        whisper "Homebrew not found, installing..."
+        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
+
     # Grab brew deps
     whisper "Brewing..."
     brew bundle install
@@ -24,6 +43,22 @@ setup_macos() {
     whisper "Setting up key repeat in VS Code"
     defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
     defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false
+
+    whisper "Setting Dock preferences"
+    # Set the Dock on the right side
+    defaults write com.apple.dock pinning -string end
+    defaults write com.apple.dock orientation -string right
+    # Autohide
+    defaults write com.apple.dock autohide -bool true
+    # Magnification size
+    defaults write com.apple.dock magnification -bool true
+    defaults write com.apple.dock largesize -float 128
+    # Show hidden apps
+    defaults write com.apple.dock showhidden -bool true
+    sudo killall Dock
+
+    whisper "Re-enabling Gatekeeper"
+    sudo spctl --master-enabl
 }
 
 say "Updating submodules"
@@ -32,8 +67,8 @@ git submodule update --init --recursive
 # OS-specific setup
 case $(uname -a) in
     [Dd]arwin*)
-        setup_fish
         setup_macos
+        setup_fish
         ;;
 
     [Ll]inux*)
